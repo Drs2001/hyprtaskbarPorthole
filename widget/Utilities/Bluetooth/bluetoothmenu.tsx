fd5ee34bf93592d2f1f5e1bb5cf66de107897497
Drs2001 <Dylanspence705607@gmail.com>
2025-10-20 17:45:22 -0400

    bluetooth is working, but throws error in try catch block, need to move adaptor control to a singleton

--- widget/Utilities/Bluetooth/bluetoothmenu.tsx
+++ widget/Utilities/Bluetooth/bluetoothmenu.tsx
@@ -2,36 +2,118 @@ import { Gtk } from 'ags/gtk4';
 import GLib from "gi://GLib"
 import Bluetooth from "gi://AstalBluetooth"
 import DeviceRow from "./devicerow"
+import { timeout } from "ags/time"
 
 export default function BluetoothMenu(backButton: Gtk.Button) {
     const bluetooth = Bluetooth.get_default()
     const adapter = bluetooth.get_adapter()
+    const connectedDevices: Bluetooth.Device[] = []
+    const pairedDevices: Bluetooth.Device[] = []
+    const discoveredDevices: Bluetooth.Device[] = []
 
     // Wrap button and content into a vertical box for tray
     const mainBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 6 })
 
     // Top bar containing the back button, title, and bluetooth toggle
-    const topBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 6 })
-    topBox.append(backButton)
-    topBox.append(new Gtk.Label({label: "Bluetooth", halign: Gtk.Align.START}))
+    const titleBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 6 })
+    titleBox.append(backButton)
+    titleBox.append(new Gtk.Label({label: "Bluetooth", halign: Gtk.Align.START}))
 
     // Middle section containing the paired devices list
-    const middleBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 6 })
-    middleBox.append(new Gtk.Label({label: "Paired devices", halign: Gtk.Align.START}))
-    for(const device of bluetooth.get_devices()){
-        if(device.get_paired()){
-            const deviceRow = DeviceRow(device)
-            middleBox.append(deviceRow)
+    const pariedDevicesBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 6 })
+    pariedDevicesBox.append(new Gtk.Label({label: "Paired devices", halign: Gtk.Align.START}))
+
+    // Bottom section containing the non-paired device list
+    const discoveredDevicesBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 6 })
+    discoveredDevicesBox.append(new Gtk.Label({label: "Not paired", halign: Gtk.Align.START}))
+
+    try {
+        adapter?.start_discovery();
+    } catch (err) {
+        print(`Failed to start discovery: ${err}`);
+    }
+
+    // Function to get paired and connected devices and populate their lists
+    function getPairedDevices(){
+        // Reset arrays
+        connectedDevices.length = 0
+        pairedDevices.length = 0
+        for(const device of bluetooth.get_devices()){
+            if(device.get_paired()){
+                if(device.get_connected()){
+                    connectedDevices.push(device)
+                }
+                else{
+                    pairedDevices.push(device)
+                }
+            }
         }
     }
 
-    // Bottom section containing the non-paired device list
-    const bottomBox = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 6 })
-    bottomBox.append(new Gtk.Label({label: "Not paired", halign: Gtk.Align.START}))
+    // Gets a list of discovered devices
+    function getDiscoveredDevices(){
+        // Reset arrays
+        discoveredDevices.length = 0
+        for(const device of bluetooth.get_devices()){
+            if(!device.get_paired()){
+                discoveredDevices.push(device)
+            }
+        }
+    }
 
-    mainBox.append(topBox)
-    mainBox.append(middleBox)
-    mainBox.append(bottomBox)
+    // Refreshes the paired devices list
+    function refreshPairedDevices(){
+        getPairedDevices()
+        // Empty the box
+        const title = pariedDevicesBox.get_first_child()
+        let child = title?.get_next_sibling() || null
+        while(child){
+            const next = child.get_next_sibling()
+            pariedDevicesBox.remove(child)
+            child = next
+        }
+        for(const device of connectedDevices){
+            const deviceRow = DeviceRow(device, refreshPairedDevices)
+            pariedDevicesBox.append(deviceRow)
+        }
+        for(const device of pairedDevices){
+            const deviceRow = DeviceRow(device, refreshPairedDevices)
+            pariedDevicesBox.append(deviceRow)
+        }
+    }
+
+    // Refreshes the discovered devices list
+    function refreshDiscoveredDevices(){
+        const title = discoveredDevicesBox.get_first_child()
+        let child = title?.get_next_sibling() || null
+        while(child){
+            const next = child.get_next_sibling()
+            discoveredDevicesBox.remove(child)
+            child = next
+        }
+
+        getDiscoveredDevices()
+        for(const device of discoveredDevices){
+            const deviceRow = DeviceRow(device, refreshAllDevices)
+            discoveredDevicesBox.append(deviceRow)
+        }
+    }
+
+    // Refreshes both lists
+    function refreshAllDevices(){
+        refreshPairedDevices()
+        refreshDiscoveredDevices()
+    }
+
+    // Get initial paired devices
+    refreshPairedDevices()
+
+    // Get initial discovered devices
+    refreshDiscoveredDevices()
+
+    mainBox.append(titleBox)
+    mainBox.append(pariedDevicesBox)
+    mainBox.append(discoveredDevicesBox)
 
     // === Wrap mainBox in a scrolled window ===
     const scroller = new Gtk.ScrolledWindow({
@@ -44,6 +126,15 @@ export default function BluetoothMenu(backButton: Gtk.Button) {
 
     // Optional: force a fixed size for the popover content
     scroller.set_size_request(-1, 400) // width x height in pixels
+
+    adapter?.connect("notify::discovering", () =>{
+        refreshDiscoveredDevices()
+    })
+
+    // TODO when the menu opens
+    scroller.connect("map", () =>{
+        console.log("OPEN")
+    })
 
 
     return scroller
--- widget/Utilities/Bluetooth/devicerow.tsx
+++ widget/Utilities/Bluetooth/devicerow.tsx
@@ -1,22 +1,61 @@
 import { Gtk } from "ags/gtk4";
+import { timeout } from "ags/time"
 import Bluetooth from "gi://AstalBluetooth";
 
-export default function DeviceRow(device: Bluetooth.Device): Gtk.Widget {
+export default function DeviceRow(
+  device: Bluetooth.Device,
+  refreshDevices?: (() => void) | null
+): Gtk.Widget {
+  const deviceName = device.get_name() || "Unknown Device";
+
+  const statusLabel = new Gtk.Label({
+    label: device.get_connected() ? "Connected" : "Disconnected",
+    halign: Gtk.Align.START,
+    xalign: 0,
+  });
+
   // The action button inside the revealer
   const actionButton = new Gtk.Button({
     label: device.get_connected() ? "Disconnect" : "Connect",
     halign: Gtk.Align.END,
   });
 
+  let timer: ReturnType<typeof timeout> | null = null;
+
   actionButton.connect("clicked", () => {
     if (device.get_connected()) {
-    //   device.disconnect_service();
-      actionButton.set_label("Connect");
+      try {
+        device.disconnect_device(null);
+      } catch (err) {
+        print(`Failed to connect: ${err}`);
+      }
+      
     } else {
-    //   device.connect_service();
-      actionButton.set_label("Disconnect");
+      try {
+        device.connect_device(null);
+        statusLabel.label = "Conecting..."
+        timer = timeout(10000, () => {
+          if(!device.get_connected()){
+            statusLabel.label = "Connection failed"
+          }
+          timer = null
+        })
+      } catch (err) {
+        print(`Failed to connect: ${err}`);
+      }
     }
   });
+
+  // This doesn't just notify when connected but for any connection change
+  device.connect("notify::connected", () => {
+    if(device.get_connected()){
+      if(timer){
+        timer.cancel()
+        timer = null
+      }
+      if (refreshDevices) refreshDevices();
+    }
+  })
 
   // Revealer that animates the button sliding down
   const revealer = new Gtk.Revealer({
@@ -26,11 +65,25 @@ export default function DeviceRow(device: Bluetooth.Device): Gtk.Widget {
   });
   revealer.set_child(actionButton);
 
+  const titleLabel = new Gtk.Label({
+    label: deviceName,
+    halign: Gtk.Align.START,
+    xalign: 0,
+  });
+
+  const contentBox = new Gtk.Box({
+    orientation: Gtk.Orientation.VERTICAL,
+    spacing: 2,
+  });
+  contentBox.append(titleLabel);
+  contentBox.append(statusLabel);
+
   // Header button for the device name
   const header = new Gtk.Button({
-    label: device.get_name() || "Unknown Device",
+    child: contentBox,
     halign: Gtk.Align.FILL,
   });
+
   header.connect("clicked", () => {
     // Toggle visibility of the action button
     revealer.reveal_child = !revealer.reveal_child;
